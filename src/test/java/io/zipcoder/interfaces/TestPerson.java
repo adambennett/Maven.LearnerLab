5055968d6fc692d6df50a1a306cdd49b0abe2631
Nyoxide <nyoxidetwitter@gmail.com>
2020-03-02 15:41:04 -0500

    sjmesh

--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,22 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestPerson {
+
+    @Test
+    public void testConstructor() {
+        Person test = new Person(0, "Adam");
+        Assert.assertTrue(test.getId() == 0);
+        Assert.assertTrue(test.getName().equals("Adam"));
+    }
+
+    @Test
+    public void testSetName() {
+        Person test = new Person(0, "Adam");
+        test.setName("Bobbie");
+        Assert.assertTrue(test.getName().equals("Bobbie"));
+    }
 
 }
---
+++ src/test/java/io/zipcoder/interfaces/TestStudent.java
@@ -0,0 +1,27 @@
+package io.zipcoder.interfaces;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestStudent {
+
+    @Test
+    public void testImplementation() {
+        Student test = new Student(0, "Adam");
+        Assert.assertTrue(test instanceof Learner);
+    }
+
+    @Test
+    public void testInheritance() {
+        Student test = new Student(0, "Adam");
+        Assert.assertTrue(test instanceof Person);
+    }
+
+    @Test
+    public void testLearn() {
+        Student test = new Student(0, "Adam");
+        test.learn(10);
+        Assert.assertEquals(new Double(10.0), test.getTotalStudyTime());
+    }
+
+}
