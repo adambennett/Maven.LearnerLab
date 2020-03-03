7b31a3aea90f928f340be9a4f76abe99635ce139
Nyoxide <nyoxidetwitter@gmail.com>
2020-03-03 13:54:56 -0500

    progress

--- src/test/java/io/zipcoder/interfaces/TestStudents.java
+++ src/test/java/io/zipcoder/interfaces/TestStudents.java
@@ -1,12 +1,18 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestStudents {
 
     @Test
     public void test() {
-
+        //Students inst = Students.getInstance();
+        Student adam = new Student(0, "Adam");
+        int expected = 29;
+        int actual = Students.getInstance().count();
+        Assert.assertEquals(expected, actual);
+        Assert.assertTrue(Students.getInstance().contains(adam));
     }
 
 }
